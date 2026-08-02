beac0a5425b99152f9775693f5be41017fc43776
Kritika Mottadi <kritikamottadi@gmail.com>
2026-08-02 13:32:09 +0530

    Day 1:introduction to python

---
+++ day01_intro/day01_exercise.py
@@ -0,0 +1,13 @@
+# Day 1: Introduction to Python
+# Exercise: Print a welcome message and basic info
+
+print("Hello, World!")
+print("Welcome to 30 Days of Python!")
+
+# Exercise: Print your name and today's date
+name = "Kritika"
+print("My name is", name)
+
+# Exercise: Simple arithmetic
+print("5 + 3 =", 5 + 3)
+print("10 * 2 =", 10 * 2)

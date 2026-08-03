6e3a46774e6232435efb28de55ff9f83d7cd813b
Kritika Mottadi <kritikamottadi@gmail.com>
2026-08-03 10:31:55 +0530

    Day 2:variable exercise

---
+++ day02_variables/variables_exercises.py
@@ -0,0 +1,54 @@
+#day 2: 30 days of python programming
+
+    
+    
+
+first_name='Kritika'
+last_name='Mottadi'
+full_name= first_name + ' ' + last_name
+country='India'
+age=20
+year=2026
+is_married=False
+is_true=True
+is_light_on=True
+fruit,vegtable,colour='cherry','potato','pink'
+
+#exercise 2
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(fruit))
+print(type(vegtable))
+print(type(colour))
+
+print(len(first_name))
+print(len(last_name))
+print(len(full_name))
+print(len(country))
+print(len(fruit))
+print(len(vegtable))
+print(len(colour))
+
+num_1=5
+num_2=4
+sum=num_1+num_2
+diff=num_1-num_2
+mul=num_1*num_2
+div=num_1/num_2
+mod=num_1%num_2
+exp=num_1**num_2
+floor_div=num_1//num_2
+
+radius=int(input("Enter the radius of a circle: "))
+area=3.14*radius**2
+circumference=2*3.14*radius
+print("Area of the circle is:", area)
+print("Circumference of the circle is:", circumference)
+
